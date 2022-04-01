581911bb2f974715488b4b651b335c52e47db8e0
CaramelSuperman <mellao_10@hotmail.com>
2022-04-01 19:45:09 -0400

    added a root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  # get "/index" , to:"index"
+  root "home#index"
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
