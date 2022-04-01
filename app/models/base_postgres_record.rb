b0cd1e67fed025e4835bf4cf754b15b48730e0a1
CaramelSuperman <mellao_10@hotmail.com>
2022-04-01 18:32:25 -0400

    changed postgres connection to hash

--- app/models/base_postgres_record.rb
+++ app/models/base_postgres_record.rb
@@ -4,5 +4,12 @@ class BasePostgresRecord < ActiveRecord::Base
     conn = PG::Connection.open(:host => "codeboxx-postgresql.cq6zrczewpu2.us-east-1.rds.amazonaws.com", :user => "codeboxx", :password => "Codeboxx1!", :dbname => "EricGaudreault")
 
     # establish_connection POSTGRES_DB
-    establish_connection conn
+    establish_connection  {  
+        adapter: "postgresql",
+        encoding: "utf8",
+        pool: 5,
+        host:  "codeboxx-postgresql.cq6zrczewpu2.us-east-1.rds.amazonaws.com",
+        username: "codeboxx",
+        password: "Codeboxx1!"
+    }
 end
