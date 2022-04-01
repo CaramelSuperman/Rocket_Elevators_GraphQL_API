f6a3adcc938ab62493cb8dc373c1e2da44b718ab
CaramelSuperman <mellao_10@hotmail.com>
2022-04-01 18:18:19 -0400

    changed pg gem connection

--- app/models/base_postgres_record.rb
+++ app/models/base_postgres_record.rb
@@ -1,4 +1,8 @@
 class BasePostgresRecord < ActiveRecord::Base
     self.abstract_class = true
-    establish_connection POSTGRES_DB
+
+    conn = PG::Connection.open(:host => "codeboxx-postgresql.cq6zrczewpu2.us-east-1.rds.amazonaws.com", :user => "codeboxx", :password => "Codeboxx1!", :dbname => "EricGaudreault")
+
+    # establish_connection POSTGRES_DB
+    establish_connection conn
 end
