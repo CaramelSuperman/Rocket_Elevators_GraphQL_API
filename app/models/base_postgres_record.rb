bf6ba4e27ebcce880f140111701750b039b820cb
CaramelSuperman <mellao_10@hotmail.com>
2022-04-01 18:38:55 -0400

    adjusted postgresql connection with pg gem

--- app/models/base_postgres_record.rb
+++ app/models/base_postgres_record.rb
@@ -1,15 +1,24 @@
 class BasePostgresRecord < ActiveRecord::Base
+    
+#     .establish_connection( 
+#     adapter: "postgresql",
+#     encoding: "utf8",
+#     pool: 5,
+#     host:  "codeboxx-postgresql.cq6zrczewpu2.us-east-1.rds.amazonaws.com",
+#     username: "codeboxx",
+#     password: "Codeboxx1!"
+# )
     self.abstract_class = true
 
     conn = PG::Connection.open(:host => "codeboxx-postgresql.cq6zrczewpu2.us-east-1.rds.amazonaws.com", :user => "codeboxx", :password => "Codeboxx1!", :dbname => "EricGaudreault")
 
     # establish_connection POSTGRES_DB
-    establish_connection  {  
-        adapter: "postgresql",
-        encoding: "utf8",
-        pool: 5,
-        host:  "codeboxx-postgresql.cq6zrczewpu2.us-east-1.rds.amazonaws.com",
-        username: "codeboxx",
-        password: "Codeboxx1!"
-    }
+    # Base.establish_connection( 
+    #     adapter: "postgresql",
+    #     encoding: "utf8",
+    #     pool: 5,
+    #     host:  "codeboxx-postgresql.cq6zrczewpu2.us-east-1.rds.amazonaws.com",
+    #     username: "codeboxx",
+    #     password: "Codeboxx1!"
+    # )
 end
