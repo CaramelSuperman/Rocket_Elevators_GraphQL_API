9054fa027e5a54700406c7e47c3814d8476904e9
CaramelSuperman <mellao_10@hotmail.com>
2022-04-01 19:03:33 -0400

    config var fixed

--- app/models/base_postgres_record.rb
+++ app/models/base_postgres_record.rb
@@ -10,9 +10,9 @@ class BasePostgresRecord < ActiveRecord::Base
 # )
     self.abstract_class = true
 
-    conn = PG::Connection.open(:host => "codeboxx-postgresql.cq6zrczewpu2.us-east-1.rds.amazonaws.com", :user => "codeboxx", :password => "Codeboxx1!", :dbname => "EricGaudreault")
+    # conn = PG::Connection.open(:host => "codeboxx-postgresql.cq6zrczewpu2.us-east-1.rds.amazonaws.com", :user => "codeboxx", :password => "Codeboxx1!", :dbname => "EricGaudreault")
 
-    # establish_connection POSTGRES_DB
+    establish_connection ENV['DATABASE_SECOND_URL']
     # Base.establish_connection( 
     #     adapter: "postgresql",
     #     encoding: "utf8",
