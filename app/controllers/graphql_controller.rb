458d78e990bc4c77bf614328dde892c2b2c54f12
CaramelSuperman <mellao_10@hotmail.com>
2022-04-01 21:17:59 -0400

    comment uncomment done

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,6 +1,6 @@
 class GraphqlController < ApplicationController
 
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
   # If accessing from outside this domain, nullify the session
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
